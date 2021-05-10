35c8786e2a5cd0834dbc18e2ccb8162cce3a9511
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-10 10:44:38 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - HERITAGE/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_01___HERITAGE.Entities {
+    //------------------------------- START -------------------------------//
+    class Account {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+    //------------------------------- CONST -------------------------------//  
+        public Account() { }
+
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+    //------------------------------- MET --------------------------------//
+        public void Withdraw(double amount) {
+            Balance -= amount;
+        }
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+    }
+    //-------------------------------- END -------------------------------//
+}
---
+++ EXERCISE 01 - HERITAGE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EXERCISE_01___HERITAGE.Entities;
+namespace EXERCISE_01___HERITAGE {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+            //account.Balance = 1500; ---- > Não permite pq esta protejido, por protected.
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
